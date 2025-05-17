f157cf9a9f4c7730a734060a06085b8e1c4d3114
Adanankhan <adnanp3799@gmail.com>
2025-05-17 16:19:17 +0530

    Final Commit

--- src/main/java/com/example/BookMyShow/dto/TicketDto.java
+++ src/main/java/com/example/BookMyShow/dto/TicketDto.java
@@ -1,20 +1,26 @@
 package com.example.BookMyShow.dto;
 
-import lombok.*;
+import com.example.BookMyShow.dto.ResponseDto.ShowResponseDto;
+import com.example.BookMyShow.dto.ResponseDto.UserResponseDto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Builder
 public class TicketDto {
 
-    int id; //ticket id
-    Set<String> allottedSeats;
+    int id;
+
+    String alloted_seats;
+
     double amount;
 
-    ShowDto showDto;
-    UserDto userDto;
+    ShowResponseDto showDto;
+
+    @NotNull
+    UserResponseDto userDto; //Mandatory for me to fill this userDto Value
 }
